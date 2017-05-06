c1261cf8301d949fdb1c8c825645a1c8951c9c7b
Ilya Purouski <MiKrob09@gmail.com>
2017-05-06 19:35:32 +0300

    addClassScrollTop() - добавлен параметр - условие на реверс добавления/удаления класса

--- src/assets/js/partials/functions/addClassScrollTop.js
+++ src/assets/js/partials/functions/addClassScrollTop.js
@@ -6,9 +6,25 @@
 /// @prop: target {string} - объект, к которому применяется данная функция.
 /// @prop: offsetTop {integer} - расстояния от верха в px, - точнка срабатывания.
 /// @prop: classAdd {string} - класс, который добавляется при условии == true.
+/// @prop: revers {bool} (default = false) - флаг, реверс  при условии == true.
 /// (класс, идентификатор или селектор).
 
-function addClassScrollTop (target, offsetTop, classAdd) {
+function addClassScrollTop (target, offsetTop, classAdd, revers) {
+    /* Переписать нормально */
+    if( revers ) {
+
+        if($(document).scrollTop() > offsetTop) {
+            if( $(target).hasClass(classAdd) )
+                $(target).removeClass(classAdd);
+        }
+        else {
+            if( !$(target).hasClass(classAdd) )
+                $(target).addClass(classAdd);
+        }
+
+        return;
+    }
+
     if($(document).scrollTop() > offsetTop) {
         if( !$(target).hasClass(classAdd) )
             $(target).addClass(classAdd);
