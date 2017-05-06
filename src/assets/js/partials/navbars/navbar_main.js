ff68f7c61d068d7034b675fa16558a8f59a78b22
Ilya Purouski <MiKrob09@gmail.com>
2017-05-06 15:10:27 +0300

    добавляет класс навигационному меню при скролле через заданное расстояние.

--- src/assets/js/partials/navbars/navbar_main.js
+++ src/assets/js/partials/navbars/navbar_main.js
@@ -1,1 +1,15 @@
-/*windows.currentScrollPosition*/
+"use strict";
+
+$(function() {
+
+    /// @description: добавляет класс навигационному меню при скролле через заданное расстояние.
+    /// @require: jQuery 1.7+.
+    /// @require: addClassScrollTop(target, offsetTop, classAdd).
+    $.extend(true, $(document).scroll(), $(document).scroll(
+        function() {
+            addClassScrollTop ("#navbar_main", 100, 'navbar_main_mini');
+        }
+    ));
+});
+
+
