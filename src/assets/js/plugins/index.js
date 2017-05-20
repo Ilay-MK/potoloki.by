eb8e8f6633d361d340c43a26bb87da911f35ea6a
Ilya Purouski <MiKrob09@gmail.com>
2017-05-20 23:46:46 +0300

    Добавлено расширение jsAnimCSSAutoRem - Автоудаление классов анимации animate.css после проигрывания анимации для animate.css

--- src/assets/js/plugins/index.js
+++ src/assets/js/plugins/index.js
@@ -2,4 +2,5 @@
 //= fn_plug_fancybox.js
 //= fn_plug_scrollTo.js
 //= fn_plug_maskedinput.js
+//= jsAnimCSSAutoRem.js
 //= maps/index.js
---
+++ src/assets/js/plugins/jsAnimCSSAutoRem.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/// Автоудаление классов анимации animate.css после проигрывания анимации.
+/// @require: animate.css.
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
