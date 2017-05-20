4879c243d67e0e0943c2b150334ee839a8bbe952
Ilya Purouski <MiKrob09@gmail.com>
2017-05-20 03:09:28 +0300

    Добавлено сворачивание меню при клике на пункты меню или логотип

--- src/assets/js/partials/navbars/index.js
+++ src/assets/js/partials/navbars/index.js
@@ -1,6 +1,8 @@
 //= contacts__item_navbar_mini.js
 //= contacts_navbar_mini.js
 //= navbar_main.js
+//= navbar-brand.js
 //= navbar-brand_mini.js
 //= navbar-brand__descript_mini.js
+//= navbar-nav__item.js
 //= navbar-nav_mini.js
---
+++ src/assets/js/partials/navbars/navbar-brand.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/// @name: -=[ navbarBrand ]=-.
+/// @description: сворачиваем меню при клике по пункту меню.
+/// @require: jQuery 1.7+.
+/// @require: Bootstrap 3.x.
+/// @access: public.
+/// @type: function.
+/// @prop: target {string} - объект, событие клика которого биндится.
+/// @prop: menu {string} - объект (меню), к которому применяется колапс.
+/// @return: none.
+
+var navbarBrand = function (target, menu) {
+    /// Событие click() с отменой перехода по ссылке
+    $(target).click(function (e) {
+        e.preventDefault();
+
+        $(menu).collapse('hide');
+    });
+};
+
+$(function() {
+    navbarBrand(".navbar-brand", "#menu_main");
+});
