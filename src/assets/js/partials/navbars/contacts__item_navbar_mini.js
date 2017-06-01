9a6f33b41097c1eb13779534f57d33406995382d
Ilya Purouski <MiKrob09@gmail.com>
2017-06-01 17:48:43 +0300

    Переработаны контакты, в частности в навигации.

--- src/assets/js/partials/navbars/contacts__item_navbar_mini.js
+++ src/assets/js/partials/navbars/contacts__item_navbar_mini.js
@@ -8,6 +8,9 @@ $(function() {
     $.extend(true, $(document).scroll(), $(document).scroll(
         function() {
             addClassScrollTop (".contacts__item_navbar", window.globOffsetTop, 'contacts__item_navbar_mini');
+            addClassScrollTop ("#navbar_main .contacts__lable_phone", window.globOffsetTop, 'contacts__lable_mini');
+            addClassScrollTop ("#navbar_main .contacts__lable_email", window.globOffsetTop, 'contacts__lable_mini');
+            addClassScrollTop ("#navbar_main .str-delimiter", window.globOffsetTop, 'str-delimiter_mini');
         }
     ));
 });
