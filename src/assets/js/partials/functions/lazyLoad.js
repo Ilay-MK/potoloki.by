d4801fa216689dbf142f1b85b40430dafb0a484b
Ilya Purouski <MiKrob09@gmail.com>
2017-07-01 11:04:07 +0300

    lazyLoad  - добавлено описание и отформатировано

--- src/assets/js/partials/functions/lazyLoad.js
+++ src/assets/js/partials/functions/lazyLoad.js
@@ -2,20 +2,20 @@
 
 /// @name: -=[ lazyLoad ]=-.
 /// @description: Загрузка картинки по событию.
-/// @require: .
-/// @access: access.
+/// @require: imgLoaded().
+/// @access: public.
 /// @type: function.
 /// @return: none.
 
-function lazyLoad(){
+function lazyLoad() {
     var $images = $('.lazy_load');
 
-    $images.each(function(){
+    $images.each( function() {
         var $img = $(this),
             src = $img.attr('data-src');
 
         $img
-            .on('load',imgLoaded($img[0]))
-            .attr('src',src);
+            .on('load', imgLoaded( $img[0] ))
+            .attr('src', src);
     });
 };
